72c250dff5d7aae185277761123980c13fa44f54
Oleksandr Kotliarov <oleksandrkotliarov@marc-o-polo.com>
2024-04-01 15:38:46 +0200

    Use token from state

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,10 @@ export default function Homepage() {
   const generateText = async () => {
     setIsLoading(true);
     const reply = await getChatCompletion(
-      `Write me exciting story in easy ${language}, that equivalent to ${level} language level. Don't say additional comments. Format text as array of sentences in JSON, using this format: [ {"${language}": "text",  "English": "text"} ... ]. Dont wrap in json code format.`
+      `Write me exciting story in easy ${language}, that equivalent to ${level} language level. Don't say additional comments. Format text as array of sentences in JSON, using this format: [ {"${language}": "text",  "English": "text"} ... ]. Dont wrap in json code format.`,
+      {
+        token: chatApiToken,
+      }
     );
     try {
       const sentences = JSON.parse(reply.content ?? "");
